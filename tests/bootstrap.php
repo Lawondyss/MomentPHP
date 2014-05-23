eb9ccc9d52d2edda735e500f0833506337468044
Ladislav Vondracek <lawondyss@gmail.com>
2014-05-24 01:03:25 +0200

    Changed directory tree by PSR-0.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../src/MomentPHP.php';
+require __DIR__ . '/../src/MomentPHP/MomentPHP.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 if (!class_exists('Tester\Assert')) {
